6429387d6ecf55c26158d93f5a686415035738a2
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-05-07 20:11:03 +0200

    Ensure React-Bootstrap works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from "./components/Header/Header";
-import Container from "./components/Container/Container";
-import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Navigation from "./components/Navigation/Navigation";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <Navigation />
       <Container>
         <Header />
       </Container>
