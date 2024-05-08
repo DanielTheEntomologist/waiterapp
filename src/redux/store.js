53a215d3e8c633664f469153fc55e72ae6c19c31
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-05-08 22:28:37 +0200

    Add initialState.js and store.js

---
+++ src/redux/store.js
@@ -0,0 +1,29 @@
+import { createStore, combineReducers } from "redux";
+import initialState from "./initialState";
+
+// import listsReducer from "./listsRedux";
+// import columnsReducer from "./columnsRedux";
+// import cardsReducer from "./cardsRedux";
+// import searchTermReducer from "./searchTermRedux";
+
+// //reducers
+// const subreducers = {
+//   lists: listsReducer,
+//   columns: columnsReducer,
+//   cards: cardsReducer,
+//   searchTerm: searchTermReducer,
+// };
+
+// const reducer = combineReducers(subreducers);
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
