721e79b58881a463a67f2d46fc28f12efa8f92d9
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-05-12 23:59:27 +0200

    Create TableForm fields and validation

--- src/components/TableForm/TableForm.js
+++ src/components/TableForm/TableForm.js
@@ -1,31 +1,121 @@
 import styles from "./TableForm.module.scss";
 
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const TableForm = () => {
   // const { id, description } = table;
   const { tableId } = useParams();
   const id = tableId;
 
+  const [status, setStatus] = useState("free");
+  const [people, setPeople] = useState(0);
+  const [peopleMax, setPeopleMax] = useState(10);
+  const [bill, setBill] = useState(0);
+
+  const validatePeopleMax = (value) => {
+    const newValue = parseInt(value);
+    console.log("newValue", newValue);
+    if (isNaN(newValue)) {
+      return setPeopleMax(0);
+    } else if (newValue < 0) {
+      return setPeopleMax(0);
+    } else if (newValue > 10) {
+      return setPeopleMax(10);
+    } else {
+      return setPeopleMax(newValue);
+    }
+  };
+  const validatePeople = (value) => {
+    const newValue = parseInt(value);
+    console.log("newValue", newValue);
+    if (isNaN(newValue)) {
+      return setPeople(0);
+    } else if (newValue < 0) {
+      return setPeople(0);
+    } else if (newValue > peopleMax) {
+      return setPeople(peopleMax);
+    } else {
+      return setPeople(newValue);
+    }
+  };
+  const validateBill = (value) => {
+    const newValue = Number(value);
+    console.log("newValue", newValue);
+    if (isNaN(newValue)) {
+      return setBill(0);
+    } else if (newValue < 0) {
+      return setBill(0);
+    } else {
+      return setBill(newValue);
+    }
+  };
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <div className="h3 d-flex justify-content-start p-3">Table {id} </div>
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <div className="h3 d-flex justify-content-start p-3">Table {id} </div>
+      <Form.Group className="mb-3">
+        <InputGroup>
+          <InputGroup.Text className="">Status</InputGroup.Text>
+          <Form.Select
+            aria-label="Table status select"
+            defaultValue={status}
+            onChange={(e) => {
+              e.preventDefault();
+              setStatus(e.target.value);
+            }}
+          >
+            <option value="free">Free</option>
+            <option value="reserved">Reserved</option>
+            <option value="busy">Busy</option>
+            <option value="cleaning">Cleaning</option>
+          </Form.Select>
+        </InputGroup>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      <InputGroup className="mb-3">
+        <InputGroup.Text className="">People</InputGroup.Text>
+        <Form.Control
+          type="number"
+          value={people}
+          onChange={(e) => {
+            e.preventDefault();
+            validatePeople(e.target.value);
+          }}
+        />
+        <Form.Control
+          type="number"
+          value={peopleMax}
+          onChange={(e) => {
+            e.preventDefault();
+            validatePeopleMax(e.target.value);
+          }}
+        />
+      </InputGroup>
+      <InputGroup className="mb-3">
+        <InputGroup.Text className="">Bill: $</InputGroup.Text>
+        <Form.Control
+          type="number"
+          placeholder="0.00"
+          value={bill}
+          onChange={(e) => {
+            e.preventDefault();
+            validateBill(e.target.value);
+          }}
+        />
+      </InputGroup>
+
+      <Form.Group className="mb-3">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
       <Button variant="primary" type="submit">
